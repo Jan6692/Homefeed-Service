57efbcf61721396bb9663a2c900ab7723ab652f3
Jan6692 <losacker.j@gmail.com>
2026-07-04 16:30:36 +0200

    add spring boot project skeleton

---
+++ src/main/java/com/br/inspo/Homefeed_Service/HomefeedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.br.inspo.Homefeed_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomefeedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomefeedServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/inspo/Homefeed_Service/HomefeedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.inspo.Homefeed_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomefeedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
