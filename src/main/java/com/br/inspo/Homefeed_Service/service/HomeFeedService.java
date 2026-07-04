b9f1deead083c8b2ce33b742c78d6b5760a2255d
Jan6692 <losacker.j@gmail.com>
2026-07-04 22:24:30 +0200

    add sample access of UserRepository

--- src/main/java/com/br/inspo/Homefeed_Service/service/HomeFeedService.java
+++ src/main/java/com/br/inspo/Homefeed_Service/service/HomeFeedService.java
@@ -4,17 +4,24 @@ import com.br.inspo.Homefeed_Service.module.FeedModule;
 import com.br.inspo.Homefeed_Service.module.GreetingFeedModule;
 import com.br.inspo.Homefeed_Service.module.ProductTeaserModule;
 import com.br.inspo.Homefeed_Service.module.SaleBannerModule;
+import com.br.inspo.Homefeed_Service.repository.UserRepository;
 import com.br.inspo.Homefeed_Service.response.HomeFeedResponse;
+import com.br.inspo.Homefeed_Service.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Service which delivers all methods needed to get homefeed related data.
  */
 @Service
 public class HomeFeedService {
+
+    @Autowired
+    private UserRepository userRepository;
 
     /**
      * Builds and returns the homefeed for a given user.
@@ -24,6 +31,7 @@ public class HomeFeedService {
      */
     public List<FeedModule> getHomeFeedResponse(String userId) {
         List<FeedModule> modules = new ArrayList<>();
+        Optional<User> optionalUser = userRepository.findById(2L);
         modules.add(new GreetingFeedModule("Deutsch", "Hallo Tania!"));
         modules.add(new ProductTeaserModule("breuninger.de/products/pictures/shirt", 29.99));
         modules.add(new SaleBannerModule("Deutsch", "SALE!", "Jetzt shoppen", "breuninger.de/ad/pictures/sale"));
