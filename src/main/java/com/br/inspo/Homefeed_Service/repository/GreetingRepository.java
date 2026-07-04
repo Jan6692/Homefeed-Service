f857ae8ff2e2d2bc69817b1283d6bd62cacf5ac0
Jan6692 <losacker.j@gmail.com>
2026-07-04 22:27:50 +0200

    add GreetingRepository

---
+++ src/main/java/com/br/inspo/Homefeed_Service/repository/GreetingRepository.java
@@ -0,0 +1,13 @@
+package com.br.inspo.Homefeed_Service.repository;
+
+import com.br.inspo.Homefeed_Service.entity.Greeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GreetingRepository extends JpaRepository<Greeting, Long> {
+
+    List<Greeting> findByLanguage(String language);
+}
