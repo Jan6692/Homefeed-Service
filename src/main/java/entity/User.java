4d0c76bf3d6a58977de160dc38e8032c5e7eb50f
Jan6692 <losacker.j@gmail.com>
2026-07-04 20:48:45 +0200

    add User entity

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,24 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "app_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "language", nullable = false)
+    private String language;
+}
+
