b37b6bf211c597e220415c11b61d9b8d95836e18
Jan6692 <losacker.j@gmail.com>
2026-07-04 22:26:48 +0200

    add ProductRepository

---
+++ src/main/java/com/br/inspo/Homefeed_Service/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.br.inspo.Homefeed_Service.repository;
+
+import com.br.inspo.Homefeed_Service.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByPriceLessThanEqual(double price);
+}
